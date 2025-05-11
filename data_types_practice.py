aeafc5c22b55742582fdb28edf0c17e920d1e76d
Dawood Iftakhar <dawood.iftakhar87@gmail.com>
2025-05-11 11:37:31 +0500

    Add files via upload

---
+++ data_types_practice.py
@@ -0,0 +1,27 @@
+# Integer Example
+age=37
+score=95
+print("Age:",age)
+print("Score:",score)
+
+# Float Example
+weight="65.5kg"
+height="5.9ft"  
+print("Weight:",weight)
+print("Height:",height)
+
+# String Example   
+name="Dawood"
+address="Islamabad"
+contact="03345085603"   
+message="Hello, my name is Dawood. I live in Islamabad and my contact number is 03345085603."
+print("Name:",name)
+print("Address:",address)
+print("Contact:",contact)
+print("Message:",message)
+
+# Boolean Example
+is_student=True 
+is_employed=False
+print("Is Student:",is_student)     
+print("Is Employed:",is_employed)
